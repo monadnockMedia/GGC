c8a6af634c9ee3ee788c66772f2fa302e074607a
Ray Ortgiesen <ray.ortgiesen@gmail.com>
2015-03-06 13:17:37 -0500

    Upload from WS

--- client/app/preview/preview.controller.js
+++ client/app/preview/preview.controller.js
@@ -4,40 +4,40 @@
 
 angular.module('ggcApp')
 
-  .controller('PreviewCtrl', function ($scope,$http,hotkeys,ggcUtil) {
+  .controller('PreviewCtrl', function ($scope,$http,hotkeys,ggcUtil, dealer) {
 	$scope.preview = {};
 	$scope.preview.hideNavbar = false;
-	$scope.preview.previewStates = ["cards","icons","screen", "test"];
+	$scope.preview.previewStates = ["cards","icons", "screen", "test"];
     $scope.preview.currentCard = 0;
 	$scope.printObject = function(o){
 		return JSON.stringify(o, null, 3);
 	};
-	
+
 
 	$scope.trust = ggcUtil.trustSVG;
-	
 
-	
+
+
 });
 
 /// controller for Card preview
 
 angular.module('ggcApp')
   .controller('CardCtrl', function ($scope,$http,hotkeys,ggcUtil) {
-	
-	
+
+
     $scope.currentCard = 0;
 	$scope.printObject = function(o){
 		return JSON.stringify(o, null, 3);
 	};
-	
+
 	ggcUtil.getCards().then(function(res){
 		console.log("cards",res.data);
 		$scope.preview.cards = res.data;
 	});
-	
 
-	
+
+
 	hotkeys.bindTo($scope)
 	    .add({
 	      combo: 'left',
@@ -58,12 +58,12 @@ angular.module('ggcApp')
 			combo: 'N',
 			description: "Hide NavBar",
 			callback: function(){
-					
+
 					$scope.preview.hideNavbar = !$scope.preview.hideNavbar;
 				}
-			
+
 		})
-	
+
 });
 
 /// controller for Icon preview
@@ -74,15 +74,15 @@ angular.module('ggcApp')
 			combo: 'N',
 			description: "Hide NavBar",
 			callback: function(){
-					
+
 					$scope.preview.hideNavbar = !$scope.preview.hideNavbar;
 				}
-			
+
 		})
 	ggcUtil.getIcons().then(function(res){
 		$scope.preview.icons = res.data;
 	});
-	
+
 });
 
 
@@ -93,22 +93,22 @@ angular.module('ggcApp')
 		$scope.game = dealer.game;
 		///dealer.hands contains the current "card" views for each player, as well as the main player
 		$scope.hands = dealer.hands;
-		
+
 		$scope.test = {
 			cards: [],
 			players: dealer.players
 		};
-		
-		
+
+
 		hotkeys.bindTo($scope)
 		.add({
 			combo: 'D',
 			description: "Draw Two",
 			callback: function(){
-					
+
 						dealer.drawTwo("economy");
 				}
-			
+
 		})
 		.add({
 			combo: 'M',
@@ -116,35 +116,74 @@ angular.module('ggcApp')
 			callback: function(){
 					console.log("hands:",JSON.stringify($scope.hands,null,3), "game: ",JSON.stringify($scope.game,null,3));
 				}
-			
+
 		})
 
-	
+
 });
+
 angular.module('ggcApp')
   .controller('ScreenCtrl', function ($scope,$http,hotkeys,dealer) {
-	
+    $scope.dealer = dealer;
+    $scope.game = dealer.game;
+    ///dealer.hands contains the current "card" views for each player, as well as the main player
+    $scope.hands = dealer.hands;
+
+		var visibiltyAccess=false, enviroPanelExtended=false, econoPanelExtended=false, energyPanelExtended=false;
+
 		$scope.test = {
 			cards: [],
 			players: dealer.players
 		};
-		
-	
-		
+
+		$scope.toggleEnviro = function() {
+			if (!enviroPanelExtended) {
+				$('#enviroPanelInner').addClass('extended');
+				enviroPanelExtended = true;
+			} else {
+				$('#enviroPanelInner').removeClass('extended');
+				enviroPanelExtended = false;
+			}
+		}
+
+		$scope.toggleEcon = function() {
+			if (!econoPanelExtended) {
+				$('#econoPanelInner').addClass('extended');
+				econoPanelExtended = true;
+			} else {
+				$('#econoPanelInner').removeClass('extended');
+				econoPanelExtended = false;
+			}
+		}
+
+		$scope.toggleEnergy = function() {
+			if (!energyPanelExtended) {
+				$('#energyPanelInner').addClass('extended');
+				energyPanelExtended = true;
+			} else {
+				$('#energyPanelInner').removeClass('extended');
+				energyPanelExtended = false;
+			}
+		}
+
+
+
 		hotkeys.bindTo($scope)
 		.add({
 			combo: 'D',
 			description: "Draw Two",
 			callback: function(){
-					
-						dealer.drawTwo("economy").then(function(d){
-							console.log("DRAW_TWO",d);
-							$scope.test.cards = d;
-						});
+					dealer.drawTwo("economy");
 				}
-			
+			})
+		.add({
+			combo: 'M',
+			description: "Print Model",
+			callback: function(){
+				console.log("hands:",JSON.stringify($scope.hands,null,3), "game: ",JSON.stringify($scope.game,null,3));
+			}
 		})
 
-	
+
 });
 
--- client/app/preview/preview.js
+++ client/app/preview/preview.js
@@ -22,7 +22,7 @@ angular.module('ggcApp')
 	.state('preview.screen', {
         url: '/screen',
         templateUrl: 'app/preview/preview.screen.html',
-        controller: 'ScreenCtrl', //CardCtrl
+        controller: 'ScreenCtrl',
 	  })
 	.state('preview.test', {
         url: '/test',
@@ -31,48 +31,3 @@ angular.module('ggcApp')
       })
 	
   });
-
-	// JavaScript Document
-	var visibiltyAccess=false, enviroPanelExtended=false, econoPanelExtended=false, energyPanelExtended=false;
-
-	function initializeMe() {
-		$( document ).ready(function() {
-
-			$('#enviroPanelInner').on('mouseup', function(e){ toggleEnviroPanel()} );
-			$('#econoPanelInner').on('mouseup', function(e){ toggleEconoPanel()} );
-			$('#energyPanelInner').on('mouseup', function(e){ toggleEnergyPanel()} );
-
-		});
-
-	}
-
-	function toggleEnviroPanel() {
-		 //clickB.play();
-		if (!enviroPanelExtended) {
-			$('#enviroPanelInner').addClass('extended');
-			enviroPanelExtended = true;
-		} else {
-			$('#enviroPanelInner').removeClass('extended');
-			enviroPanelExtended = false;
-		}
-	}
-	function toggleEconoPanel() {
-		 //clickB.play();
-		if (!econoPanelExtended) {
-			$('#econoPanelInner').addClass('extended');
-			econoPanelExtended = true;
-		} else {
-			$('#econoPanelInner').removeClass('extended');
-			econoPanelExtended = false;
-		}
-	}
-	function toggleEnergyPanel() {
-		 //clickB.play();
-		if (!energyPanelExtended) {
-			$('#energyPanelInner').addClass('extended');
-			energyPanelExtended = true;
-		} else {
-			$('#energyPanelInner').removeClass('extended');
-			energyPanelExtended = false;
-		}
-	}
