5fd780906cd7e5ff0b042583a2596ef46b94cc6f
W. Ryan Nestor <nestor@fuzzy-math.com>
2015-03-09 12:06:28 -0400

    game test

--- client/app/preview/preview.controller.js
+++ client/app/preview/preview.controller.js
@@ -3,122 +3,121 @@
 ///Main preview controller
 
 angular.module('ggcApp')
-
-  .controller('PreviewCtrl', function ($scope,$http,hotkeys,ggcUtil, dealer) {
-	$scope.preview = {};
-	$scope.preview.hideNavbar = false;
-	$scope.preview.previewStates = ["cards","icons", "test"];
+  .controller('PreviewCtrl', function ($scope, $http, hotkeys, ggcUtil, $rootScope) {
+    $scope.preview = {};
+    $scope.preview.hideNavbar = false;
+    $scope.preview.previewStates = ["cards", "icons", "test"];
     $scope.preview.currentCard = 0;
-	$scope.printObject = function(o){
-		return JSON.stringify(o, null, 3);
-	};
-	
+    $scope.printObject = function (o) {
+      return JSON.stringify(o, null, 3);
+    };
 
-	$scope.trust = ggcUtil.trustSVG;
-	
+    $scope.trust = ggcUtil.trustSVG;
 
-	
-});
+    $rootScope.$on('$stateChangeStart',
+      function(event, toState, toParams, fromState, fromParams){
+        debugger;
+      })
+
+  });
 
 /// controller for Card preview
 
 angular.module('ggcApp')
-  .controller('CardCtrl', function ($scope,$http,hotkeys,ggcUtil) {
-	
-	
-    $scope.currentCard = 0;
-	$scope.printObject = function(o){
-		return JSON.stringify(o, null, 3);
-	};
-	
-	ggcUtil.getCards().then(function(res){
-		console.log("cards",res.data);
-		$scope.preview.cards = res.data;
-	});
-	
+  .controller('CardCtrl', function ($scope, $http, hotkeys, ggcUtil) {
 
-	
-	hotkeys.bindTo($scope)
-	    .add({
-	      combo: 'left',
-	      description: 'Previous Card',
-	      callback: function(){
-				$scope.preview.currentCard = ($scope.preview.currentCard == 0) ? 0 : $scope.preview.currentCard-1 ;
-			}
-	    })
-		.add({
-	      combo: 'right',
-	      description: 'Next Card',
-	      callback: function(){
-				console.log("Current card test: ",$scope.preview.currentCard + 1, $scope.preview.cards.length )
-				$scope.preview.currentCard = Math.min($scope.preview.currentCard + 1, $scope.preview.cards.length-1);
-			}
-	    })
-		.add({
-			combo: 'N',
-			description: "Hide NavBar",
-			callback: function(){
-					
-					$scope.preview.hideNavbar = !$scope.preview.hideNavbar;
-				}
-			
-		})
-	
-});
+    $scope.currentCard = 0;
+    $scope.printObject = function (o) {
+      return JSON.stringify(o, null, 3);
+    };
+
+    ggcUtil.getCards().then(function (res) {
+      console.log("cards", res.data);
+      $scope.preview.cards = res.data;
+    });
+
+
+    hotkeys.bindTo($scope)
+      .add({
+        combo: 'left',
+        description: 'Previous Card',
+        callback: function () {
+          $scope.preview.currentCard = ($scope.preview.currentCard == 0) ? 0 : $scope.preview.currentCard - 1;
+        }
+      })
+      .add({
+        combo: 'right',
+        description: 'Next Card',
+        callback: function () {
+          console.log("Current card test: ", $scope.preview.currentCard + 1, $scope.preview.cards.length)
+          $scope.preview.currentCard = Math.min($scope.preview.currentCard + 1, $scope.preview.cards.length - 1);
+        }
+      })
+      .add({
+        combo: 'N',
+        description: "Hide NavBar",
+        callback: function () {
+
+          $scope.preview.hideNavbar = !$scope.preview.hideNavbar;
+        }
+
+      })
+
+  });
 
 /// controller for Icon preview
 angular.module('ggcApp')
-  .controller('IconCtrl', function ($scope,$http,hotkeys,ggcUtil) {
-		hotkeys.bindTo($scope)
-		.add({
-			combo: 'N',
-			description: "Hide NavBar",
-			callback: function(){
-					
-					$scope.preview.hideNavbar = !$scope.preview.hideNavbar;
-				}
-			
-		})
-	ggcUtil.getIcons().then(function(res){
-		$scope.preview.icons = res.data;
-	});
-	
-});
+  .controller('IconCtrl', function ($scope, $http, hotkeys, ggcUtil) {
+    hotkeys.bindTo($scope)
+      .add({
+        combo: 'N',
+        description: "Hide NavBar",
+        callback: function () {
+
+          $scope.preview.hideNavbar = !$scope.preview.hideNavbar;
+        }
+
+      })
+    ggcUtil.getIcons().then(function (res) {
+      $scope.preview.icons = res.data;
+    });
+
+  });
 
 
 //Ryan's Test controller
 angular.module('ggcApp')
-  .controller('TestCtrl', function ($scope,$http,hotkeys,dealer) {
-		$scope.dealer = dealer;
-		$scope.game = dealer.game;
-		///dealer.hands contains the current "card" views for each player, as well as the main player
-		$scope.hands = dealer.hands;
-		
-		$scope.test = {
-			cards: [],
-			players: dealer.players
-		};
-		
-		
-		hotkeys.bindTo($scope)
-		.add({
-			combo: 'D',
-			description: "Draw Two",
-			callback: function(){
-					
-						dealer.drawTwo("economy");
-				}
-			
-		})
-		.add({
-			combo: 'M',
-			description: "Print Model",
-			callback: function(){
-					console.log("hands:",JSON.stringify($scope.hands,null,3), "game: ",JSON.stringify($scope.game,null,3));
-				}
-			
-		})
+  .controller('TestCtrl', function ($scope, $http, hotkeys, dealer) {
+    $scope.dealer = dealer;
+    $scope.game = dealer.game;
+    ///dealer.hands contains the current "card" views for each player, as well as the main player
+    $scope.hands = dealer.hands;
 
-	
-});
+    $scope.test = {
+      cards: [],
+      players: dealer.players
+    };
+
+
+    hotkeys.bindTo($scope)
+      .add({
+        combo: 'D',
+        description: "Draw Two",
+        callback: function () {
+
+          dealer.drawTwo("economy");
+        }
+
+      })
+      .add({
+        combo: 'M',
+        description: "Print Model",
+        callback: function () {
+          console.log("hands:", JSON.stringify($scope.hands, null, 3), "game: ", JSON.stringify($scope.game, null, 3));
+        }
+
+      })
+
+
+  });
 
