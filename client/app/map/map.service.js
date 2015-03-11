b46ab919c1af3dc5bd3eb57ab23512a1905a22ac
cupofnestor <nestor@fuzzy-math.com>
2015-03-11 11:04:34 -0400

    adding grid directive

--- client/app/map/map.service.js
+++ client/app/map/map.service.js
@@ -4,17 +4,21 @@ angular.module('ggcApp')
   .service('ggcMapper', function ( $q) {
     // AngularJS will instantiate a singleton by calling "new" on this function
     this.grid = {
-      points: [], hexes: [], hexRadius: 0
+      points: [], hexes: [], hexRadius: 0, width:0,height:0,rows:0,columns:0
     }
 
     var grid = this.grid;
 
-    this.buildHexes = function(columns, rows, width, height){
+    this.buildHexes = function(columns, rows, _w, _h){
       var dfd = $q.defer();
+      grid.width = _w;
+      grid.height = _h;
+      grid.columns = columns;
+      grid.rows = rows;
 
       grid.hexRadius = d3.min(
-        [width/((columns + 0.5) * Math.sqrt(3)),
-          height/((rows + 1/3) * 1.5)]
+        [_w/((columns + 0.5) * Math.sqrt(3)),
+          _h/((rows + 1/3) * 1.5)]
       );
 
       this.hexBin = d3.hexbin()
@@ -22,7 +26,7 @@ angular.module('ggcApp')
 
       for (var i = 0; i < rows; i++) {
         for (var j = 0; j < columns; j++) {
-          var pt = [grid.hexRadius * j * 1.75, grid.hexRadius * i * 1.5];
+          var pt = [grid.hexRadius * j * 1.75 + grid.hexRadius, grid.hexRadius * i * 1.5 + grid.hexRadius];
           grid.points.push(pt);
         }//for j
       }//for i
--- client/app/sandbox/sandbox.controller.js
+++ client/app/sandbox/sandbox.controller.js
@@ -8,8 +8,12 @@ angular.module('ggcApp')
 
 angular.module('ggcApp')
   .controller('GridCtrl', function ($scope, ggcMapper) {
+    $scope.grid = {};
+
     ggcMapper.buildHexes(16,10,800,500).then(function(d){
-      console.log("MAPPER", d);
+      $scope.grid = ggcMapper.grid;
+      $scope.hexBin = ggcMapper.hexBin;
+      $scope.grid.hexScale = 0.8;
     })
 
   });
