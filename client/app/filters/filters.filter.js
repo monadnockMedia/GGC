6fc4eee3ae69c078123d01228123bc5ecd3a1474
Ray Ortgiesen <ray.ortgiesen@gmail.com>
2015-03-16 10:51:56 -0400

    Creating /game Route
    Meshes the graphics with Ryan’s game logic.

--- client/app/filters/filters.filter.js
+++ client/app/filters/filters.filter.js
@@ -14,3 +14,36 @@ app.filter('capitalize', function() {
     return input.substring(0,1).toUpperCase()+input.substring(1);
   }
 });
+
+app.filter('supercap', function() {
+
+  return function(input, scope) {
+    if (input!=null) {
+      var i, j, lowers, uppers;
+      input = this.replace(/([^\W_]+[^\s-]*) */g, function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+      });
+
+      // Certain minor words should be left lowercase unless
+      // they are the first or last words in the string
+      lowers = ['A', 'An', 'The', 'And', 'But', 'Or', 'For', 'Nor', 'As', 'At',
+        'By', 'For', 'From', 'In', 'Into', 'Near', 'Of', 'On', 'Onto', 'To', 'With'];
+      for (i = 0, j = lowers.length; i < j; i++)
+        input = input.replace(new RegExp('\\s' + lowers[i] + '\\s', 'g'),
+          function(txt) {
+            return txt.toLowerCase();
+          });
+
+      // Certain words such as initialisms or acronyms should be left uppercase
+      uppers = ['Id', 'Tv'];
+      for (i = 0, j = uppers.length; i < j; i++)
+        input = input.replace(new RegExp('\\b' + uppers[i] + '\\b', 'g'),
+          uppers[i].toUpperCase());
+
+
+      return input;
+    }
+
+  }
+
+});
---
+++ client/app/game/game.controller.spec.js
@@ -0,0 +1,23 @@
+'use strict';
+
+describe('Controller: GameCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('ggcApp'));
+
+  var GameCtrl, scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    GameCtrl = $controller('GameCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should ...', function () {
+    expect(1).toEqual(1);
+  });
+});
+
+
