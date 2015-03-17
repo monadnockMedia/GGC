0fa649219b00d1f41fcf1ebc9f5dea90ad4171ac
cupofnestor <nestor@fuzzy-math.com>
2015-03-17 14:07:51 -0400

    can manipulate svg content

--- client/app/ggcIcon/ggcIcon.directive.js
+++ client/app/ggcIcon/ggcIcon.directive.js
@@ -16,17 +16,19 @@ angular.module('ggcApp')
 
         attrs.$observe('iconId', function(value) {
           scope.trust = ggcUtil.trustSVG;
-          console.log("observe", value);
           ggcUtil.getIcons(value).then(function (res) {
             var icon = res.data;
             //TODO(Ryan)  add further DOM manipulation
-           /* scope.compIcon = ggcUtil.trustSVG($compile(icon.icon)(scope, function(e,s){
-              var svg = d3.select(e.toArray());
+            var iconElement = angular.element(icon.icon);
 
-            })[0].outerHTML); */
 
+           var svg = d3.selectAll(iconElement);
+            svg.append("circle").attr({
+              cx: 150, cy: 150, r: 70, stroke: "blue", fill: "none"
+            })
+
+            scope.compIcon = iconElement[0].outerHTML;
             scope.icon = icon;
-            console.log("ICON",res.data);
           });
           //some action
         });
