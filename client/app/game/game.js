9811f8158598f880df0e67dc2aaec88e5f312877
cupofnestor <nestor@fuzzy-math.com>
2015-03-25 23:18:28 -0400

    added icons as directives, cleaning up css-insanity

--- client/app/game/game.js
+++ client/app/game/game.js
@@ -7,5 +7,18 @@ angular.module('ggcApp')
         url: '/game',
         templateUrl: 'app/game/game.html',
         controller: 'GameCtrl'
+      })
+      .state('game.play', {
+        url: '/play',
+        views: {
+          "scrim": {templateUrl: 'app/game/scrim.html'},
+          "plane": {templateUrl: 'app/game/plane.html'}
+        }
+      })
+      .state('game.play.loop', {
+        url: '/loop',
+        views: {
+          "main@game": {templateUrl: 'app/game/main_loop.html'}
+        }
       });
-  });
+  })
