1944afd618bf5c29b93c18eafce93b8785859c8f
cupofnestor <nestor@fuzzy-math.com>
2015-04-01 11:59:24 -0400

    hotkey/button handling

--- client/app/game/game.controller.js
+++ client/app/game/game.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('ggcApp')
-  .controller('GameCtrl', function ($scope, $http, ggcUtil, $rootScope, dealer) {
+  .controller('GameCtrl', function ($scope, $http, ggcUtil, $rootScope, dealer, hotkeys) {
     $scope.preview = {};
     $scope.preview.hideNavbar = false;
 
@@ -17,6 +17,53 @@ angular.module('ggcApp')
     $scope.game = dealer.game;
     ///dealer.hands contains the current "card" views for each player, as well as the main player
     $scope.hands = dealer.hands;
+    bindKeys();
+    function bindKeys(){
 
+      hotkeys.bindTo($scope)
+        .add({
+          combo: '1',
+          description: 'Environment Select 1',
+          callback: function(){
+            dealer.playerChoice("environment",0);
+          }
+        })
+        .add({
+          combo: '2',
+          description: 'Environment Select 2',
+          callback: function(){
+            dealer.playerChoice("environment",1);
+          }
+        })
 
+        .add({
+          combo: '3',
+          description: 'Economy Select 1',
+          callback: function(){
+            dealer.playerChoice("economy",0);
+          }
+        })
+        .add({
+          combo: '4',
+          description: 'Environment Select 2',
+          callback: function(){
+            dealer.playerChoice("economy",1);
+          }
+        })
+
+        .add({
+          combo: '5',
+          description: 'Energy Select 1',
+          callback: function(){
+            dealer.playerChoice("energy",0);
+          }
+        })
+        .add({
+          combo: '6',
+          description: 'Energy Select 2',
+          callback: function(){
+            dealer.playerChoice("energy",1);
+          }
+        })
+    }
   });
