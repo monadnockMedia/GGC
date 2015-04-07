592189bd8ea70a5fd805f1da648a3008352fcb1a
cupofnestor <nestor@fuzzy-math.com>
2015-04-07 11:34:15 -0400

    started dealer random event

--- client/app/dealer/dealer.service.js
+++ client/app/dealer/dealer.service.js
@@ -1,28 +1,32 @@
 'use strict';
 
-angular.module('ggcApp').service('dealer', function($http, $q, $rootScope, ggcUtil, $interval, ggcMapper) {
+angular.module('ggcApp').service('dealer', function ($http, $q, $rootScope, ggcUtil, $interval, ggcMapper) {
   // AngularJS will instantiate a singleton by calling "new" on this function
 
   this.decks = {};
 
-  this.game = {main:{}, players:{}, currentPlayer:{}, playerIndex:0};
+  this.game = {main: {}, players: {}, currentPlayer: {}, playerIndex: 0};
   this.game.score = {};
   this.game.phase = "none";
+  var events = [];
   var chosenCard;
   var currentCards;
   var self = this;
-
+  var chance = 1;
   var shuffle = ggcUtil.shuffle;
   console.log("DEALER");
   ///
   ///Setup the "fresh" deck
   ///
-  $http.get('/api/cards/grouped').then(function(r) {
+  $http.get('/api/cards/grouped').then(function (r) {
     self.players = Object.keys(r.data);
     self.freshDecks = r.data;
 
     init();
   });
+  ggcUtil.getEvents().then(function (r) {
+    events = r.data;
+  })
 
 
 ///utility functions
@@ -34,12 +38,12 @@ angular.module('ggcApp').service('dealer', function($http, $q, $rootScope, ggcUt
     self.game.totalScore = 0;
 
     //setup the game by players
-    eachPlayer(function(k){
+    eachPlayer(function (k) {
       pushDeck(k);
-      var rand = ~~(Math.random()*4+3);
+      var rand = ~~(Math.random() * 4 + 3);
       self.game.totalScore += rand;
-      self.game.score[k]= {i:rand,p:0};
-      self.game.players[k] = { hand:{choices:[],issue:{}}, docked:true};
+      self.game.score[k] = {i: rand, p: 0};
+      self.game.players[k] = {hand: {choices: [], issue: {}}, docked: true};
     })
 
     calculatePercentage();
@@ -48,43 +52,50 @@ angular.module('ggcApp').service('dealer', function($http, $q, $rootScope, ggcUt
     phases.setup();
   }
 
-  function calculatePercentage(){
+  function roll() {
+    return (chance == 0) ? false : ~~(Math.random() * chance) == 0;
+  }
+
+  function randomEvent() {
+    return events[~~(Math.random() * events.length)];
+  }
+
+  function calculatePercentage() {
     var score = self.game.score;
-    eachPlayer(function(p){
-      score[p].p = score[p].i/self.game.totalScore;
+    eachPlayer(function (p) {
+      score[p].p = score[p].i / self.game.totalScore;
     });
   }
 
-  function nextPlayer(){
+  function nextPlayer() {
     var i = self.game.playerIndex;
-    self.game.playerIndex = (i == 2 ) ? 0 : i+1;
-    setCurrentPlayer(self.game.playerIndex );
+    self.game.playerIndex = (i == 2 ) ? 0 : i + 1;
+    setCurrentPlayer(self.game.playerIndex);
   }
 
-  function eachPlayer( f ){
+  function eachPlayer(f) {
     self.players.forEach(f);
   }
 
-  function setCurrentPlayer( i ){
+  function setCurrentPlayer(i) {
     //p is current player name
     var p = self.players[i];
     self.game.currentPlayer = p;
-    eachPlayer(function(pp){
+    eachPlayer(function (pp) {
       //for each player, if they are not current, set docked to true;
-      self.game.players[pp].currentPlayer = (pp==p);
-      self.game.players[pp].docked = !(pp==p);
-      debugger;
+      self.game.players[pp].currentPlayer = (pp == p);
+      self.game.players[pp].docked = !(pp == p);
 
     })
   }
 
   ///reset the hands
-  function buildHands( player ){
+  function buildHands(player) {
     self.game.main = [];
 
     eachPlayer(nullHand);
 
-    function nullHand(p){
+    function nullHand(p) {
       self.game.players[p].voted = false;
       self.game.players[p].hand = {
         choices: null,
@@ -105,11 +116,11 @@ angular.module('ggcApp').service('dealer', function($http, $q, $rootScope, ggcUt
   }
 
   ///Score
-  function tally(){
+  function tally() {
 
     var score = self.game.score;
 
-    eachPlayer(function(p){
+    eachPlayer(function (p) {
       score[p].i += chosenCard.effects[p].score;
       self.game.totalScore += score[p].i;
     });
@@ -117,14 +128,17 @@ angular.module('ggcApp').service('dealer', function($http, $q, $rootScope, ggcUt
     calculatePercentage();
     addIcon(chosenCard.icon);
   }
-  function addIcon(icon){
-    ggcMapper.putIcon(ggcMapper.randomIndex(),icon._id)
+
+  function addIcon(icon) {
+    ggcMapper.putIcon(ggcMapper.randomIndex(), icon._id)
 
   }
+
+
 ///Phases
   var phases = {};
 
-  phases.setup = function(){
+  phases.setup = function () {
     self.game.phase = "setup";
     self.game.votes = {};
     self.game.totalScore = 0;
@@ -132,20 +146,20 @@ angular.module('ggcApp').service('dealer', function($http, $q, $rootScope, ggcUt
     self.drawTwo(self.game.currentPlayer);
   }
 
-  phases.choice = function() {
+  phases.choice = function () {
     self.game.phase = "choice";
 
     console.log("PHASE CHOICE");
     //loop through the two drawn cards
-    var cPlayerCards = currentCards.map(function(c, i) {
+    var cPlayerCards = currentCards.map(function (c, i) {
       var copy = JSON.parse(JSON.stringify(c));
       //sneakily sort out player cards in the loop
-      eachPlayer(function(k) {
+      eachPlayer(function (k) {
         //if this is the current player
         if (k == self.game.currentPlayer) {
           //build the choice view
           var playerEffects = copy.effects[k];
-          self.game.players[k].hand.choices[i] =  {
+          self.game.players[k].hand.choices[i] = {
             action: playerEffects.action,
             icon: playerEffects.icon
           };
@@ -157,32 +171,40 @@ angular.module('ggcApp').service('dealer', function($http, $q, $rootScope, ggcUt
     self.game.main = cPlayerCards;
   }
 
-  phases.vote = function( i ) {
+  phases.vote = function (i) {
     self.game.phase = "vote";
 
 
-
-
-    eachPlayer(function(k) {
+    eachPlayer(function (k) {
       var playerEffects = chosenCard.effects[k];
       self.game.players[k].hand.issue = playerEffects;
       self.game.players[k].docked = false;
     });
   }
 
-  phases.scoring = function( passed ){
+  phases.scoring = function (passed) {
     self.game.phase = "scoring";
-    if(passed) tally();
+    if (passed) tally();
     nextPlayer();
-    var timer = $interval(function(){
+    var timer = $interval(function () {
       $interval.cancel(timer);
-      phases.setup();
-    }, 2000);
+      phases.setup()
+      //(roll()) ? phases.event() : phases.setup();
+    }, 500);
+  }
+
+  phases.event = function () {
+    self.game.phase = "event";
+    self.game.main = randomEvent();
+  }
+
+  phases.afterMath = function(){
+    self.game.phase = "aftermath";
   }
 
 ///dealer methods
   //pull two cards from the deck
-  this.drawTwo = function(team) {
+  this.drawTwo = function (team) {
     currentCards = [];
     if (this.decks[team].length <= 4) {
       pushDeck(team);
@@ -198,9 +220,9 @@ angular.module('ggcApp').service('dealer', function($http, $q, $rootScope, ggcUt
   }
 
   ///currentPlayer Chooses an issue
-  this.choose = function(p, i) {
+  this.choose = function (p, i) {
     //set "chosen" on players choice card to true
-    if(p == self.game.currentPlayer){
+    if (p == self.game.currentPlayer) {
       self.game.players[self.game.currentPlayer].hand.choices[i].chosen = true;
       chosenCard = currentCards[i];
 
@@ -214,19 +236,19 @@ angular.module('ggcApp').service('dealer', function($http, $q, $rootScope, ggcUt
   }
 
   ///called when each player votes
-  this.vote = function(p, v) {
+  this.vote = function (p, v) {
     self.game.players[p].hand.issue.vote = v;
     self.game.votes[p] = v;
     self.game.players[p].voted = true;
     var keys = Object.keys(self.game.votes);
 
-    if(keys.length == 3){
+    if (keys.length == 3) {
       var i = 3;
       var ct = 0;
-      while(i--){
-        ct+=self.game.votes[keys[i]];
+      while (i--) {
+        ct += self.game.votes[keys[i]];
       }
-      var passed = (ct>=2);
+      var passed = (ct >= 2);
 
       console.log(
         (passed) ? "PASSED" : "FAILED", ct
@@ -235,12 +257,12 @@ angular.module('ggcApp').service('dealer', function($http, $q, $rootScope, ggcUt
     }
   };
 
-  this.playerChoice = function(p, b){
+  this.playerChoice = function (p, b) {
 
-    if(self.game.phase == "choice"){
-      self.choose(p,b);
-    }else if (self.game.phase == "vote"){
-      self.vote(p,b);
+    if (self.game.phase == "choice") {
+      self.choose(p, b);
+    } else if (self.game.phase == "vote") {
+      self.vote(p, b);
     }
 
   };
