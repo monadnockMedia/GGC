e2d3ceceadfffc8c908846dfa75d68eb55f42da9
cupofnestor <nestor@fuzzy-math.com>
2015-04-21 16:19:19 -0400

    GGC-25 Add SVG filters to grid icons

--- client/app/root/root.controller.js
+++ client/app/root/root.controller.js
@@ -1,68 +1,64 @@
 'use strict';
 
 
-
 angular.module('ggcApp')
   .controller('AppCtrl', function ($scope, $location, hotkeys, $http, dealer) {
+    $http.get('app/config.json').then(function (res) {
 
-	$http.get('app/config.json').then(function(res){
-
-		$scope.config = res.data;
-			console.log("CONFIG" , res);
-	})
-
+      $scope.config = res.data;
+      console.log("CONFIG", res);
+    })
 
 
     $scope.menu = [{
       'title': 'Home',
       'link': '/'
-    },{
+    }, {
       'title': 'Sandbox',
       'link': '/sandbox'
-    },{
+    }, {
       'title': 'Preview',
       'link': '/preview'
     }];
 
-	$scope.models =["Card", "Icon", "Content", "Test", "Event"];
+    $scope.models = ["Card", "Icon", "Content", "Test", "Event"];
 
     $scope.isCollapsed = true;
 
-    $scope.isActive = function(route) {
+    $scope.isActive = function (route) {
       return route === $location.path();
     };
 
-	hotkeys.add({
-	    combo: 'P',
-	    description: 'Preview Mode',
-	    callback: function() {
-	      $location.path('/preview');
-	    }
-	  });
-
-	hotkeys.add({
-	    combo: 'E',
-	    description: 'Edit Mode',
-	    callback: function() {
-	      $location.path('/edit');
-	    }
-	  });
-
-	hotkeys.add({
-	    combo: 'a',
-	    description: 'Alert Mode',
-	    callback: function() {
-	      alert("Alert");
-	    }
-	  });
     hotkeys.add({
-      combo: 'D',
-      description: 'Debug Mode',
-      callback: function() {
-        $scope.debug = true;
+      combo: 'P',
+      description: 'Preview Mode',
+      callback: function () {
+        $location.path('/preview');
       }
     });
 
+    hotkeys.add({
+      combo: 'E',
+      description: 'Edit Mode',
+      callback: function () {
+        $location.path('/edit');
+      }
+    });
+
+    hotkeys.add({
+      combo: 'a',
+      description: 'Alert Mode',
+      callback: function () {
+        alert("Alert");
+      }
+    });
+    hotkeys.add({
+      combo: 'D',
+      description: 'Debug Mode',
+      callback: function () {
+        $scope.debug = true;
+      }
+    });
 
 
   });
