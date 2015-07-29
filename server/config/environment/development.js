f728fe230d7ec11b8ce346a0d496146f7c289eb5
cupofnestor <nestor@fuzzy-math.com>
2015-07-29 15:11:13 -0400

    added ENV var to configure mongodb URI

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -6,7 +6,8 @@ module.exports = {
   // MongoDB connection options
   mongo: {
     //uri: 'mongodb://localhost/ggc-dev'
-    uri: 'mongodb://aries.local/ggc-dev'
+    uri:  process.env.MONGOD_URI ||
+          'mongodb://localhost/ggc-dev'
   },
   appConfig: require("../../../client/app/config.json"),
   seedDB: false
--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -15,7 +15,8 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri:    process.env.MONGOLAB_URI ||
+    uri:    process.env.MONGOD_URI ||
+            process.env.MONGOLAB_URI ||
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
             'mongodb://127.0.0.1/ggc'
