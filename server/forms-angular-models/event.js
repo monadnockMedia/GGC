8df462525ec159f0000cf96d4ed9bc38bf1d1735
cupofnestor <nestor@fuzzy-math.com>
2015-08-10 12:26:57 -0400

    updated model

--- server/forms-angular-models/event.js
+++ server/forms-angular-models/event.js
@@ -8,7 +8,7 @@ var EventSchema = new Schema({
   eventName: {type:String, index:true, list:true, form:{tab:"Event"}},
   videoFile: {type:String, index:false, list:false, form:{tab:"Event"}},
   text: {type: String, form: {type: 'textarea', editor: 'ckEditor', rows:"1", tab:"Event"}},
-  icon: {type: String, form: {type: 'textarea', rows:20, tab:"Event", help: "Copy and paste contents of .svg file here."}},
+  icons : { type: Schema.Types.ObjectId,  ref: 'Icon', form: { tab:"Event"} },
   effects:{
     environment:{
       message: {type: String, form: {type: 'textarea', rows:4, tab:"Environment"}},
