5cb37b9088bc821c8a52207557aaf596322d981f
cupofnestor <nestor@fuzzy-math.com>
2015-08-18 11:42:15 -0400

    added rotation to scores

--- client/app/ggcScore/ggcScore.directive.js
+++ client/app/ggcScore/ggcScore.directive.js
@@ -15,10 +15,11 @@ angular.module('ggcApp')
         //debugger;
         var l = pathEl.getTotalLength();
         var group = d3.select(element[0]);
-        var step = 35;
-
+        var step = 38;
+        var padding = {x:2,y:15};
+        var center = {x:960,y:540};
         var scoreSfx = ngAudio.load("../sound/score_slide.wav");
-
+        var angles = {"environment": -25,"economy" : 0, "energy":25};
         scope.$watch(function() {
           return scope.game.score[team];
         }, update, true);
@@ -31,42 +32,60 @@ angular.module('ggcApp')
             var scoreData = $filter("scoreIconArray")(newV.i, team);
             var previousPoint = pathEl.getPointAtLength(0);
             var nextPoint;
-            var glyphIconPOS = {environment: [575, 900], economy: [950, 1000], energy: [1370, 900]};
+            var initialPosition = {environment: {x:575,y: 900}, economy: {x:950, y:1000}, energy: {x:1370, y:900}};
 
             var changed = group.selectAll("text").data(scoreData);
             //set the starting point
             changed.enter().append("text").attr({
-              x: glyphIconPOS[team][0],
-              y: glyphIconPOS[team][1],
+              //x: initialPosition[team][0],
+              //y: initialPosition[team][1],
+              transform: function(d,i){
+                var pt = initialPosition[team];
+                return "translate("+ pt.x+" "+pt.y+" ) rotate("+(angles[team]+90)+")";
+
+              },
               class: team + " glyphIcons",
+              opacity: 0.5
             })
               .transition() //add the transition
-              //push the new attributes (could be any xhtml attributes)
+              ////push the new attributes (could be any xhtml attributes)
               .attr({
-                x: function(d,i){
-                  var newI = i+1;
-                  var pt = pathEl.getPointAtLength(newI*step);
+                //x: function(d,i){
+                //  var newI = i+1;
+                //  var pt = pathEl.getPointAtLength(newI*step);
+                //
+                //  return pt.x;
+                //},
+                //y: function(d,i){
+                //  var newI = i+1;
+                //  var pt = pathEl.getPointAtLength(newI*step);
+                //
+                //  return pt.y+15;
+                //}
+                transform: function(d,i){
+                  var pt = pathEl.getPointAtLength((i+1)*step);
+                  return "translate("+(pt.x+padding.x)+" "+(pt.y+padding.y)+" ) rotate("+angles[team]+")";
 
-                  return pt.x;
                 },
-                y: function(d,i){
-                  var newI = i+1;
-                  var pt = pathEl.getPointAtLength(newI*step);
-
-                  return pt.y+15;
-                }
+                opacity: 1
               })
+              .attr({})
               .delay(function(d,i){return i*100}) // a delay based on index
               .style("fill", glyphiconColor).style("font-size", "26px").duration(2000).text(function(d){return d});
 
             group.selectAll("text").data(scoreData).exit()
               .transition().attr({
-                x: glyphIconPOS[team][0],
-                y: glyphIconPOS[team][1],
+                //x: initialPosition[team][0],
+                //y: initialPosition[team][1],
+                transform: function(d,i){
+                  var pt = initialPosition[team];
+                  return "translate("+ pt.x+" "+pt.y+" ) rotate(-360)";
+
+                },
 
               })
               .delay(function(d,i){return i*100})
-              .style("opacity", 0.3).style("font-size", "60px").duration(1000).remove();
+              .style("opacity", 0).style("font-size", "60px").duration(1000).remove();
           }
         }
       }
