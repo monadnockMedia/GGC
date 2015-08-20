9f49ca969cdba6739cf1d4ed8f2554b15fd426df
cupofnestor <nestor@fuzzy-math.com>
2015-08-20 13:08:48 -0400

    added glow to score icons

--- client/app/ggcScore/ggcScore.directive.js
+++ client/app/ggcScore/ggcScore.directive.js
@@ -9,12 +9,13 @@ angular.module('ggcApp')
       transclude: true,
       link: function (scope, element, attrs) {
         var team = attrs.team;
+        var dur = 700;
         var iconIndex;
         var abs = $location.absUrl();
         var pathEl = element.find("path")[0];
         //debugger;
         var l = pathEl.getTotalLength();
-        var group = d3.select(element[0]);
+        var group = d3.select(element[0]).append("g");
         var step = 38;
         var padding = {x:2,y:15};
         var center = {x:960,y:540};
@@ -34,9 +35,23 @@ angular.module('ggcApp')
             var nextPoint;
             var initialPosition = {environment: {x:575,y: 900}, economy: {x:950, y:1000}, energy: {x:1370, y:900}};
 
-            var changed = group.selectAll("text").data(scoreData);
+            var changed = group.selectAll("g").data(scoreData);
             //set the starting point
-            changed.enter().append("text").attr({
+            var enterGroups = changed.enter().append("g").attr("class","iconGroup");
+            var exitGroups = changed.exit()
+
+            enterGroups.append("text").text(function(d){return d}).attr({
+              class: "scoreDupe"
+            }).style("fill", "white").style("filter","url("+abs+"#pulse)")
+              .transition().delay(dur*4).style("opacity",0).style("filter",null);
+
+            enterGroups.append("text").text(function(d){return d}).attr({
+              class: team + " glyphIcons",  id: function(d,i){return team+"Score"+i}
+            }).style("fill", glyphiconColor);
+
+
+
+            enterGroups.attr({
               //x: initialPosition[team][0],
               //y: initialPosition[team][1],
               transform: function(d,i){
@@ -50,18 +65,6 @@ angular.module('ggcApp')
               .transition() //add the transition
               ////push the new attributes (could be any xhtml attributes)
               .attr({
-                //x: function(d,i){
-                //  var newI = i+1;
-                //  var pt = pathEl.getPointAtLength(newI*step);
-                //
-                //  return pt.x;
-                //},
-                //y: function(d,i){
-                //  var newI = i+1;
-                //  var pt = pathEl.getPointAtLength(newI*step);
-                //
-                //  return pt.y+15;
-                //}
                 transform: function(d,i){
                   var pt = pathEl.getPointAtLength((i+1)*step);
                   return "translate("+(pt.x+padding.x)+" "+(pt.y+padding.y)+" ) rotate("+angles[team]+")";
@@ -71,9 +74,9 @@ angular.module('ggcApp')
               })
               .attr({})
               .delay(function(d,i){return i*100}) // a delay based on index
-              .style("fill", glyphiconColor).style("font-size", "26px").duration(2000).text(function(d){return d});
+              .duration(dur)
 
-            group.selectAll("text").data(scoreData).exit()
+            exitGroups
               .transition().attr({
                 //x: initialPosition[team][0],
                 //y: initialPosition[team][1],
@@ -85,7 +88,9 @@ angular.module('ggcApp')
 
               })
               .delay(function(d,i){return i*100})
-              .style("opacity", 0).style("font-size", "60px").duration(1000).remove();
+              .style("opacity", 0).duration(dur*4).remove();
+            exitGroups
+              .selectAll(".scoreDupe").style("fill","red").style("filter","url("+abs+"#pulse)").style("opacity","1");
           }
         }
       }
